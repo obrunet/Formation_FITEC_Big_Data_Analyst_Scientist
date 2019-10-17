29c2bba4b74f014a60b0bc34deaabb9668fd6643
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-10-17 22:36:04 +0200

    Add files via upload

---
+++ 02.Scrapping/TP03.py
@@ -0,0 +1,33 @@
+"""List all pokemons on the web page https://pokemondb.net/pokedex/national
+in order to use bs4 & requests libs"""
+
+import os, requests, pickle, html5lib
+from bs4 import BeautifulSoup
+
+
+URL = 'https://pokemondb.net/pokedex/national'
+file_name = 'scraped_pokemon_page.pickle'
+
+
+# scrape the web page then if succeed, write a binary file for later use
+if not os.path.exists(file_name):
+    result = requests.get(URL)
+    if result.status_code != 200:
+        print(f'Attempt to retrieve web page failed - result code {result.status_code}')
+    else:
+        with open(file_name, 'wb') as f:
+            pickle.dump(result, f)
+else:
+    with open(file_name, 'rb') as f:
+        result = pickle.load(f)
+
+soup = BeautifulSoup(result.content, 'html5lib')
+
+# retrieve all pokemon's names
+names = [n.string for n in soup.find_all("a", class_="ent-name")]
+with open('all_pokemons_names.txt', 'wt') as f:
+    f.write('\n'.join(names))
+
+# get all infocards with links for each element
+infocards = soup.find_all("div", class_="infocard")
+
