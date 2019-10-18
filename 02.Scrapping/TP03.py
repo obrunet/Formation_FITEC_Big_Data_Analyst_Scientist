5d6ce542f840a1ae834b631df9e6c46ec7cc179e
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-10-18 09:49:24 +0200

    Add files via upload

--- 02.Scrapping/TP03.py
+++ 02.Scrapping/TP03.py
@@ -12,22 +12,19 @@ file_name = 'scraped_pokemon_page.pickle'
 # scrape the web page then if succeed, write a binary file for later use
 if not os.path.exists(file_name):
     result = requests.get(URL)
-    if result.status_code != 200:
-        print(f'Attempt to retrieve web page failed - result code {result.status_code}')
-    else:
-        with open(file_name, 'wb') as f:
-            pickle.dump(result, f)
+    assert result.status_code == 200 > 0, f'Attempt to retrieve web page failed - code {result.status_code}'
+    with open(file_name, 'wb') as f:
+        pickle.dump(result, f)
 else:
     with open(file_name, 'rb') as f:
         result = pickle.load(f)
 
 soup = BeautifulSoup(result.content, 'html5lib')
 
-# retrieve all pokemon's names
+# retrieve all pokemon's names in a txt file
 names = [n.string for n in soup.find_all("a", class_="ent-name")]
-with open('all_pokemons_names.txt', 'wt') as f:
+with open("all_pokemons' names.txt", 'wt') as f:
     f.write('\n'.join(names))
 
 # get all infocards with links for each element
-infocards = soup.find_all("div", class_="infocard")
-
+#infocards = soup.find_all("div", class_="infocard")
