b6d749df07b9338aeff50f952e2f33c26ad2d659
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-10-23 10:30:36 +0200

    Add files via upload

---
+++ 05.Git/01.git_init.sh
@@ -0,0 +1,17 @@
+
+  162  cd Code/05.Git/
+  163  git init
+  164  ls -ahl
+  165  cd .git
+  168  ls
+  169  cd ..
+
+# global modifii la config pour tous les repo sinion utiliser local
+  171  git config --global user.name "Olivier B"
+  172  git config --global user.email o.brunet@outlook.com
+  174  git --help
+  178  git config --list
+
+  170  git config --global o.brunet "Olivier B"
+  180  git config --global --unset o.brunet
+  181  git config --list
