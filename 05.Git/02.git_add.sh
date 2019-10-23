5d7981faf6758da9ebf952cbf75c7e9167c780b3
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-10-23 14:43:07 +0200

    Add files via upload

---
+++ 05.Git/02.git_add.sh
@@ -0,0 +1,11 @@
+  245  git status
+  246  vi bis.txt
+  247  git add bis.txt 
+  248  vi bis.txt
+  249  git status
+  250  vi bis.txt
+  251  git commit bis.txt -m "test staging ou commit"
+  252  vi bis.txt
+  253  clear
+  254  git status
+  255  history > git_add.sh
