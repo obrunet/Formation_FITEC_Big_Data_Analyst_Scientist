d2e7bf88e6ca9181f3a70a0ce677604bbdc91d9b
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-10-28 16:44:06 +0100

    Add files via upload

---
+++ 07.Hadoop/03.TP2.sh
@@ -0,0 +1,29 @@
+# list files recursively
+hdfs dfs -ls -R
+
+# create a dir
+hdfs dfs -mkdir test5
+
+# create a subdir
+hdfs dfs -mkdir test5/testbis
+
+# remove recursively folder and subdirs
+hdfs dfs -rm -r test5
+
+# copy a file
+hdfs dfs -cp stocks.csv test/
+
+# see a file
+hdfs dfs -cat stocks.csv
+hdfs dfs -tail stocks.csv
+
+# retrieve a file locally
+sudo hdfs dfs -get stocks.csv test_local.csv
+
+# merge two files 
+hdfs dfs -cp stocks.csv stock_bis.csv
+# the test_local.csv file must exists before
+sudo hdfs dfs -getmerge stocks.csv stocks.csv test_local.csv 
+
+#hdfs dfs -getmerge -nl /src /opt/output.txt
+#hdfs dfs -getmerge -nl /src/file1.txt /src/file2.txt /output.txt
