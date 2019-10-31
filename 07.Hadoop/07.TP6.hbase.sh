d7ace9e74824f7a4334e17b9fd83c87bc6e473bc
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-10-31 12:45:53 +0100

    Add files via upload

--- 07.Hadoop/07.TP6.hbase.sh
+++ 07.Hadoop/07.TP6.hbase.sh
@@ -9,3 +9,45 @@ docker run --name=cloudera --hostname=quickstart.cloudera --cpus=4 -m=8g --privi
 # retry
 hbase shell
 status
+version
+whoami
+
+
+# get help
+table_help
+
+# list all tables
+list
+
+# creation
+create 'tablename','columnfamily'
+
+# status of table
+is_enabled 'tablename'
+is_disabled 'tablename'
+disable 'tablename'
+enable 'tablename'
+enable_all 'tb.*'
+diable_all 'tb.*'
+exists 'tb_classe'
+
+
+
+# drop a table
+disable 'tablename'
+drop 'tablename'
+
+# more info
+describe 'tablename'
+desc 'tablename'
+
+
+# load data
+create 'tablename', 'cf1', 'cf2'
+put 'tablename', 'rowkey1', 'cf1:column_name1', 'valueA'
+put 'tablename', 'rowkey1', 'cf1:column_name2', 'valueB'
+put 'tablename', 'rowkey2', 'cf1:column_name1', 'valueC'
+put 'tablename', 'rowkey2', 'cf1:column_name2', 'valueD'
+
+# modify value
+put 'tablename', 'rowkey1', 'cf1:column_name1', 'valueZ'
