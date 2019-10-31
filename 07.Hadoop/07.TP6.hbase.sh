5800afe999eea811ca1299fca21a39d3c8d2e104
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-10-31 14:06:47 +0100

    Add files via upload

--- 07.Hadoop/07.TP6.hbase.sh
+++ 07.Hadoop/07.TP6.hbase.sh
@@ -51,3 +51,66 @@ put 'tablename', 'rowkey2', 'cf1:column_name2', 'valueD'
 
 # modify value
 put 'tablename', 'rowkey1', 'cf1:column_name1', 'valueZ'
+
+# get the content of one line
+get ’<table name>’,’row1’
+get 'tablename', 'rowkey1'
+
+# get column family
+get 'tablename', ‘rowid’, {COLUMN => ‘column family name’}
+get 'emp', '1', {COLUMN => 'personal_data'}
+
+
+
+# print table
+scan 'tablename'
+scan 'tablename' , { VERSIONS => 4}
+scan 'test', {FILTER => "PrefixFilter('002')"}
+# ou bien
+scan 'test', STARTROW => '002', ENDROW => '003'
+
+
+# nb of lines
+count ‘<table name>’
+
+# empty table
+truncate ‘<table name>
+
+
+# change structure
+disable 'test'
+alter 'test', NAME => 'data', VERSIONS => 4
+enable 'test'
+
+
+# get value with timestamp
+get 'test', '002A', {COLUMN => 'data:details', TIMERANGE => [0, timestamp]}
+get 'test', '002A', {COLUMN => 'data:details', TIMERANGE => [0, timestamp], VERSIONS => 4}
+
+
+delete 'test', '002A', 'data:info'
+scan 'test'
+
+
+disable 'test'
+alter 'test', 'delete' => 'data'
+enable 'test'
+scan 'test'
+
+
+disable 'test'
+drop 'test'
+list
+
+
+# ------------------------------------
+
+hive> CREATE EXTERNAL TABLE
+	> hbase_table_client (id int, name string)
+	> STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
+	> WITH SERDEPROPERTIES ("hbase.columns.mapping" = ":key,cf:nom")
+	> TBLPROPERTIES ("hbase.table.name" = "client");
+
+hdfs dfs -ls /user/hive/warehouse
+
+hive> select * from hbase_table_client
