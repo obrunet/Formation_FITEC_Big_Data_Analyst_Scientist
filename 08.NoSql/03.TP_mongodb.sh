3d536e7ab8a0d00e8db6b2a6b4c77b3be04611b5
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-11-06 12:14:34 +0100

    Add files via upload

---
+++ 08.NoSql/03.TP_mongodb.sh
@@ -0,0 +1,45 @@
+> mongo
+MongoDB shell version v3.6.3
+connecting to: mongodb://127.0.0.1:27017
+MongoDB server version: 3.6.3
+
+
+
+> show test;
+#2019-11-06T11:33:48.761+0100 E QUERY    [thread1] Error: don't know how to show [test] :
+#shellHelper.show@src/mongo/shell/utils.js:953:11
+#shellHelper@src/mongo/shell/utils.js:706:15
+@(shellhelp2):1:1
+> show dbs;
+admin   0.000GB
+config  0.000GB
+local   0.000GB
+
+
+
+> db.help();
+DB methods:
+
+
+> use test;
+# create a collection
+> db.personne.insert({nom: "Wick", prenom: "John"});
+WriteResult({ "nInserted" : 1 })
+
+> show collections;
+personne
+> db
+test
+> db.personne.find()
+{ "_id" : ObjectId("5dc2a2c6e4dba17814366ad0"), "nom" : "Wick", "prenom" : "John" }
+
+> db.personne.update({"nom" : "Wick"}, {$set : {"key1": "val1"}})
+WriteResult({ "nMatched" : 1, "nUpserted" : 0, "nModified" : 1 })
+> db.personne.find()
+{ "_id" : ObjectId("5dc2a2c6e4dba17814366ad0"), "nom" : "Wick", "prenom" : "John", "key1" : "val1" }
+> db.personne.updateMany({nom: "Wick"}, {$unset: {nom:0}})
+{ "acknowledged" : true, "matchedCount" : 3, "modifiedCount" : 3 }
+> db.personne.find()
+{ "_id" : ObjectId("5dc2a2c6e4dba17814366ad0"), "prenom" : "John", "key1" : "val1" }
+{ "_id" : ObjectId("5dc2a5dfe4dba17814366ad3"), "nom" : "YOUYOU", "prenom" : "test" }
+
