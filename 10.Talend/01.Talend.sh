89341ac60526d9d9bc83e37e976b946afe72560b
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-11-18 10:36:33 +0100

    Add files via upload

---
+++ 10.Talend/01.Talend.sh
@@ -0,0 +1,4 @@
+sudo apt install openjdk-8-jre-headless
+
+# https://fr.talend.com/products/data-integration-manuals-release-notes/
+# TOS_DI-20191031_1204-V7.3.1M3.zip	7.3.1M3	4 novembre 2019	Milestone	Unix_Linux|Windows|MAC	931MB
