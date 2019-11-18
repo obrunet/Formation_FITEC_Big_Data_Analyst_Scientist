9681c3e4ad06243a788bcf5f88c72bf4cd8c2c26
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-11-18 15:04:09 +0100

    Add files via upload

--- 10.Talend/01.Talend.sh
+++ 10.Talend/01.Talend.sh
@@ -2,3 +2,14 @@ sudo apt install openjdk-8-jre-headless
 
 # https://fr.talend.com/products/data-integration-manuals-release-notes/
 # TOS_DI-20191031_1204-V7.3.1M3.zip	7.3.1M3	4 novembre 2019	Milestone	Unix_Linux|Windows|MAC	931MB
+# extraire l'archive dans un dossier talend
+
+
+# avec la version 7.2 de talend installer opendjdk v8, pour la 7.3 installer openjdk 11
+sudo apt install openjdk-11-jre
+
+# si besoin pour choisir la version de java -> https://doc.ubuntu-fr.org/openjdk
+sudo update-alternatives --config java
+
+# lancer talend
+fitec@fitec-ThinkPad-P52s:~/Talend$ ./TOS_DI-linux-gtk-x86_64
